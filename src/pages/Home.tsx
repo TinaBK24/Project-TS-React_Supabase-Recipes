08d969c99062fca758f4b5fc128c390dc6fe7f46
Khrystyna Barshchevska-Kostrova <barshchevskak@gmail.com>
2025-01-23 15:36:32 +0100

    fix home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,30 @@
-import {
-    useContext,
-    // useEffect, 
-    // useState 
-} from "react";
+import { useContext, useEffect, useState } from "react";
 import Banner from "../components/Banner";
 import PopularRecipes from "../components/PopularRecipes";
 import { UserContext } from "../context/UserContext";
-// import { fetchFavoriteRecipes } from "../utils/favoriteRecipes";
+import { fetchFavoriteRecipes } from "../utils/favoriteRecipes";
 
 const Home = () => {
     const { favRezepte, setFavRezepte, popularRecipes, setPopularRecipes } = useContext(UserContext);
-    // const [loading, setLoading] = useState<boolean>(true);
+    const [loading, setLoading] = useState<boolean>(true);
 
-    // useEffect(() => {
-    //     const loadFavorites = async () => {
-    //         await fetchFavoriteRecipes(setFavRezepte);
-    //         setLoading(false);
-    //     }
-    // }, []);
+    useEffect(() => {
+        const loadFavorites = async () => {
+            await fetchFavoriteRecipes(setFavRezepte);
+            setLoading(false);
+        }
+
+        loadFavorites();
+    }, []);
 
     return (
         <section className="bg-white">
             <Banner />
-            {/* {loading ? (
-            <p className="text-3xl font-semibold text-center pb-7">Favoriten werden geladen...</p>
-            ) : ( */}
-            <PopularRecipes favRezepte={favRezepte} setFavRezepte={setFavRezepte} popularRecipes={popularRecipes} setPopularRecipes={setPopularRecipes} />
-            {/* )} */}
+            {loading ? (
+                <p className="text-3xl font-semibold text-center pb-7">Favoriten werden geladen...</p>
+            ) : (
+                <PopularRecipes favRezepte={favRezepte} setFavRezepte={setFavRezepte} popularRecipes={popularRecipes} setPopularRecipes={setPopularRecipes} />
+            )}
         </section>
     );
 }
